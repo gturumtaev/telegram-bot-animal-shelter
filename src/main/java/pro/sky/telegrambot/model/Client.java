c090cc8b58400bfd3d7a94d912d12584df3f10e1
gerchikk <geraloi@yandex.ru>
2024-03-06 14:12:18 +0500

    Попытки для устранения

--- src/main/java/pro/sky/telegrambot/model/Client.java
+++ src/main/java/pro/sky/telegrambot/model/Client.java
@@ -11,6 +11,7 @@ import java.util.Objects;
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "INTEGER")
     private Long id;
 
     @Column(name = "first_name")
--- src/main/java/pro/sky/telegrambot/model/Shelter.java
+++ src/main/java/pro/sky/telegrambot/model/Shelter.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 public class Shelter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     private String workSchedule;
